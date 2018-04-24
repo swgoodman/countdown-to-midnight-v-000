c80413e0b6011e2622812a64487f45be03ba8d9b
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-24 02:25:36 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown(seconds)
   while seconds > 0
-    puts "#{seconds} SECOND(S)"
+    puts "#{seconds} SECOND(S)!"
     seconds -= 1
   end
   return "HAPPY NEW YEAR!"
@@ -10,7 +10,7 @@ end
 
 def countdown_with_sleep(seconds)
   while seconds > 0
-    puts "#{seconds} SECOND(S)"
+    puts "#{seconds} SECOND(S)!"
     seconds -= 1
     sleep(1)
   end
